339b3baf64d37efecc9e5dce84bea05db6d21e5a
pbalazi@gmail.com <Samsung7>
2018-07-29 22:36:44 +0200

    Many changes. Added LCD dialogues, language changing.

--- uvLampArduino/uvLampArduino.ino
+++ uvLampArduino/uvLampArduino.ino
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <EEPROM.h>
+#include <DS3231.h>
 
 // definitions
 
@@ -15,10 +16,12 @@ static int RELAY1 = 9;
 static int RELAY2 = 10;
 static int RELAY3 = 11;
 
-static String msg_en[15] = {"","","","","","","","","","","","","","",""};
+static String msg_en[15] = {"Lamp is ready","Options ->","-> Language","","","","","","","","","","","",""};
 static String msg_de[15] = {"","","","","","","","","","","","","","",""};
-static String msg_cz[15] = {"","","","","","","","","","","","","","",""};
-static String msg_it[15] = {"","","","","","","","","","","","","","",""};
+static String msg_cz[15] = {"Lampa pripravena","Nastaveni ->","-> Jazyk","","","","","","","","","","","",""};
+static String msg_fr[15] = {"","","","","","","","","","","","","","",""};
+
+static int blockMenuLimit = 400;
 
 // Structure which stores current status of exact buttons and semaphore that any button is down or not
 struct buttons {
@@ -52,7 +55,7 @@ struct buttons {
   bool anyPressed  = false;
 };
 
-// structure convering relays
+// structure covering relays
 struct relay {
   int lightPin = 0;
   bool lightOn = false;
@@ -60,7 +63,12 @@ struct relay {
 
 // structure with setup loaded from persistent ram
 struct persistentConfig {
-  int cycleDelay = 300;
+  // general delay in each cycle
+  int cycleDelay = 100;
+  // max number of seconds which could be uv lamp on - security limit
+  int maxUvTime = 600;
+  // language default
+  char curLang[3] = "en";
 };
 
 // setup instance of structure
@@ -76,6 +84,22 @@ persistentConfig cfg;
 int btnVal = 1;
 // setup lcd object
 LiquidCrystal_I2C lcd(LCD_ADDRESS,LCD_CHARS,LCD_LINES); 
+// setup rtc ds3231 object
+DS3231 rtc;
+// create variable for rtc
+RTCDateTime dateTime;
+// block menu for a second to avoid too fast switching of menu
+int blockMenuCur = 0;
+
+// -- global variables
+// start unix time when uv has been light on
+long uvStartTime = -1;
+// start unix time when light has been light on
+long lightStartTime = -1;
+// string which remembers position in menu
+String curMenuPos = "main";
+// force to refresh lcd in next cycle
+bool forceRefreshLcd = false;
 
 int sensorValue;
 
@@ -205,7 +229,7 @@ int setRelay(struct relay *rel, String action, bool checkLitButton = false, int 
       // do it - no lit check required
       digitalWrite(rel->lightPin, LOW);
       rel->lightOn = true;
-      Serial.println("DEBUG:Relay " + String(rel->lightPin) + " up - no lit check.");
+      Serial.println("DEBUG:Relay " + String(rel->lightPin) + " up - no lit check:LightOncheck:" + String(rel->lightOn));
     }
   }
   else if (action == "down") {
@@ -225,7 +249,7 @@ int setRelay(struct relay *rel, String action, bool checkLitButton = false, int 
 // show text on display
 // if entered string2, than it is expected that we should show it as two separated lines, otherwise split 
 // the line into two lines if necessary
-void showText(String line1, String line2, String effect){
+void showText(String line1, String line2, String effect = ""){
   lcd.clear();
   // if second line is explicitly
   if(line2 != ""){
@@ -238,7 +262,78 @@ void showText(String line1, String line2, String effect){
     lcd.setCursor ( 0, 0 );
     lcd.print(line1);
   }
+
+  // setup initial display screen
+  Serial.print("Temperature: ");
+  Serial.println(rtc.readTemperature());
 }
+
+// -- dispaly process functions - counting with 2 lines and 16 chars
+// print message in proper language
+String mLoc(int strId) {
+  String retStr = "";
+  Serial.println("DEBUG:mLoc");
+  Serial.print("DEBUG:mLoc:curlang="); Serial.println(cfg.curLang);
+  // current language is in cfg.curLang
+  if(strcmp(cfg.curLang,"de") == 0) {
+    Serial.print("DEBUG:mLoc:de:");Serial.println(strId);
+    retStr = msg_de[strId];
+  }
+  else if(strcmp(cfg.curLang,"cz") == 0) {
+    Serial.print("DEBUG:mLoc:cz:");Serial.println(strId);
+    retStr = msg_cz[strId];
+  }
+  else if(strcmp(cfg.curLang,"fr") == 0) {
+    Serial.print("DEBUG:mLoc:fr:");Serial.println(strId);
+    retStr = msg_fr[strId];
+  }
+  else { // en is default
+    Serial.print("DEBUG:mLoc:en:");Serial.println(strId);
+    retStr = msg_en[strId];  
+  }
+  // return localized string
+  return retStr;
+}
+// print status screen
+void printStatus() {
+  String line1 = "";
+  String line2 = "";
+  line1 = mLoc(0);
+  line2 = "          " + String(rtc.readTemperature());
+  // show on display
+  showText(line1, line2);
+}
+// -- end display process
+
+// -- block menu
+// test if menu is blocked
+bool isMenuBlocked() {
+  if(blockMenuCur > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+// reset counter to max
+void setMenuBlock() {
+  blockMenuCur = blockMenuLimit;
+}
+
+// process one interval to unblock
+void doMenuUnBlock() {
+  if(blockMenuCur > 0) {
+    blockMenuCur = blockMenuCur - cfg.cycleDelay;
+  }
+}
+// -- end block menu
+
+
+// -- EEPROm functions
+void storeConfig(){
+  EEPROM.put(EEPROM_CONFIG_ADDR, cfg);
+}
+// -- ENd EEPROm functions
 
 void setup() {
   // initialize serial communication
@@ -246,6 +341,11 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  // start rtc
+  rtc.begin();
+  // one time setup
+  // rtc.setDateTime(__DATE__, __TIME__);
 
   // load config from eeprom
   EEPROM.get(EEPROM_CONFIG_ADDR, cfg);
@@ -266,17 +366,146 @@ void setup() {
   pinMode(btns.uvPin,INPUT_PULLUP);
   pinMode(btns.lightPin,INPUT_PULLUP);
   pinMode(btns.litClosedPin,INPUT_PULLUP);
-  //digitalWrite(btns.lightPin, HIGH);
 
   // setup relays
   setupRelays();
+
+  // print status page
+  printStatus();
+  curMenuPos = "main";
+  
 }
 
 void loop() {
   // detect status of buttons set
   detectCurrentButtonsStatus();
 
-  // if up button increse the time
+  // -- MAIN menu --
+  // if se are in setup menu and would like to go back to main
+  if(curMenuPos == "main-option" && btns.leftArrow && !isMenuBlocked()) {
+    printStatus();
+    curMenuPos = "main";
+    setMenuBlock();
+  }
+  
+  // -- END MAIN menu --
+  
+  // -- OPTION menu --
+  // if we would like to go into setup
+  if(curMenuPos == "main" && btns.rightArrow && !isMenuBlocked()) {
+    showText(mLoc(1), "");
+    curMenuPos = "main-option";
+    setMenuBlock();
+  }
+  // if se are in setup menu and would like to go back to main
+  if(curMenuPos == "main-option-language" && btns.leftArrow && !isMenuBlocked()) {
+    showText(mLoc(1), "");
+    curMenuPos = "main-option";
+    setMenuBlock();
+  }
+  // -- END OPTION menu --
+
+  // -- LANGUAGE menu --
+  if(curMenuPos == "main-option" && btns.rightArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "            ...");
+    curMenuPos = "main-option-language";
+    setMenuBlock();
+  }
+
+  if(curMenuPos == "main-option-language" && forceRefreshLcd) {
+    Serial.print("DEBUG:Pressed OK:Menu go to Language");Serial.println(curMenuPos);
+    showText(mLoc(2), "            ...");
+    curMenuPos = "main-option-language";
+    setMenuBlock();
+    forceRefreshLcd = false;
+  }
+  
+  if(curMenuPos == "main-option-language" && btns.upArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "             EN");
+    curMenuPos = "main-option-language-en";
+    setMenuBlock();
+  }
+  if(curMenuPos == "main-option-language-en" && btns.upArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "             DE");
+    curMenuPos = "main-option-language-de";
+    setMenuBlock();
+  }
+  if(curMenuPos == "main-option-language-de" && btns.upArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "             CZ");
+    curMenuPos = "main-option-language-cz";
+    setMenuBlock();
+  }
+  if(curMenuPos == "main-option-language-cz" && btns.upArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "             FR");
+    curMenuPos = "main-option-language-fr";
+    setMenuBlock();
+  }
+  if(curMenuPos == "main-option-language-fr" && btns.upArrow && !isMenuBlocked()) {
+    showText(mLoc(2), "            ...");
+    curMenuPos = "main-option-language";
+    setMenuBlock();
+  }
+  // -- END LANGUAGE menu --
+  if(btns.ok) {
+    // menu handlers
+      if(curMenuPos == "main-option-language-en") {
+        strcpy(cfg.curLang,"en");
+        storeConfig();
+        curMenuPos = "main-option-language";
+      }
+      else if(curMenuPos == "main-option-language-de") {
+        strcpy(cfg.curLang,"de");
+        storeConfig();
+        curMenuPos = "main-option-language";
+      }
+      else if(curMenuPos == "main-option-language-cz") {
+        strcpy(cfg.curLang,"cz");
+        storeConfig();
+        curMenuPos = "main-option-language";
+      }
+      else if(curMenuPos == "main-option-language-fr") {
+        strcpy(cfg.curLang,"fr");
+        storeConfig();
+        curMenuPos = "main-option-language";
+      }
+    // end menu handlers
+    forceRefreshLcd  = true;
+  }
+  // -- OK actions --
+  
+  // -- END OK actions --
+  // debug print current buttons status
+  printCurrentButtonsStatus();
+
+
+  // -- UV button
+
+  // -- END UV button
+
+  // -- LIGHT button
+  if(btns.light) {
+    Serial.print("DEBUG:relay:lightOn:"); Serial.println(r1.lightOn);
+    if (r1.lightOn == 0) {
+      setRelay(&r2, "up");
+    }
+    if (r1.lightOn == 1) {
+      Serial.print("DEBUG:relay:lightOn:goDown"); Serial.println(r1.lightOn);
+      setRelay(&r2, "down");
+    }
+  }
+  // -- END LIGHT button
+  // security limit for tim e when uv lamp may be in progress
+  if(uvStartTime) {}
+
+  // delay for configured value
+  delay(cfg.cycleDelay);
+  doMenuUnBlock();
+}
+
+// debug - all code which were in phase debug, but is not used now.
+void noopfunc(){
+
+   // if up button increse the time
   if(btns.upArrow) {
     showText("UP interval",String(cfg.cycleDelay),"");
     cfg.cycleDelay = cfg.cycleDelay + 10;
@@ -291,6 +520,18 @@ void loop() {
   // if uv, than store config
   if(btns.uv) {
       EEPROM.put(EEPROM_CONFIG_ADDR, cfg);
+      dateTime = rtc.getDateTime();
+      
+      uvStartTime = dateTime.unixtime;
+      // test
+      Serial.print("Datum a cas DS3231: "); 
+      Serial.println(dateTime.unixtime);
+      Serial.print(dateTime.year);   Serial.print("-");
+      Serial.print(dateTime.month);  Serial.print("-");
+      Serial.print(dateTime.day);    Serial.print(" ");
+      Serial.print(dateTime.hour);   Serial.print(":");
+      Serial.print(dateTime.minute); Serial.print(":");
+      Serial.print(dateTime.second); Serial.println("");
   }
 
   // if press ok, detect status of lit
@@ -312,8 +553,5 @@ void loop() {
       setRelay(&r2, "down");
     }
   }
-  printCurrentButtonsStatus();
-
-  // delay for configured value
-  delay(cfg.cycleDelay);
 }
+
